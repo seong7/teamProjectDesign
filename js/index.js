79e01345a8a356e16d318c3a431e654c7174d25f
gank3362 <cho2304@naver.com>
2019-12-02 12:01:22 +0900

    12.02 second commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 function init(){
-    document.addEventListener('#menuNav', e => {
-    const header = e.detail.target;  // header became sticky or stopped sticking.
-    const sticking = e.detail.stuck; // true when header is sticky.
-    header.classList.toggle('nav_isSticky', sticking); // add drop shadow when sticking.
-  
-    document.querySelector('#menuNav').textContent = header.textContent;
-  });
+    //모달 제어부
+    let modaltrigger = false;
+    const userCheckBox = document.querySelector('#userPopup');
+    const modalWindow = document.getElementById('modalDiv');
+    const modalInnerFrame = document.getElementById('modalInnerFrame');
+
+    userCheckBox.addEventListener('change', function(){
+        if(!modaltrigger){
+            modalWindow.style.display = 'block';
+            modaltrigger = true;
+            setTimeout(function(){
+                modalInnerFrame.style.width = "600px";
+                modalInnerFrame.style.height = "800px";
+            }, 80);
+            console.log(modaltrigger);
+        }
+        else{
+            modalWindow.style.display = 'none';
+            modaltrigger = false;
+            modalInnerFrame.style.width = "0px";
+            modalInnerFrame.style.height = "0px";
+        }
+    });
 };
 
   init();
